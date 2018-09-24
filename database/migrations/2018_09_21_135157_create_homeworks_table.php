8bff298f63a529cfb3a5b0f3b282d3d15b145bd7
Max K <mxss1998@yandex.ru>
2018-09-24 12:48:01 +0300

    updated migrations & composer.json

---
+++ database/migrations/2018_09_21_135157_create_homeworks_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHomeworksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('homeworks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("string")->nullable(false);
+            $table->smallInteger("number");
+            $table->string("url");
+            $table->integer("course_id")->unsigned()->nullable(false);
+            $table->foreign('course_id')
+                ->references('id')->on('internship_courses')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamp("deadline");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('homeworks', function (Blueprint $table)
+        {
+            $table->dropForeign(['course_id']);
+        });
+        Schema::dropIfExists('homeworks');
+    }
+}
