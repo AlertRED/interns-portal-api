34e6e1880b29db0d8dd165590b96e39618778ca3
Max Kovalenko <mxss1998@yandex.ru>
2018-11-16 16:17:58 +0300

    tests updated

---
+++ app/Support/Tests/TestDataCreatorUtil.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mxss
+ * Date: 17.09.18
+ * Time: 9:34
+ */
+
+namespace App\Support\Tests;
+
+use App\User;
+use Hash;
+
+/**
+ * @method getDummyUser()
+ */
+class TestDataCreatorUtil
+{
+    /**
+     * @var User
+     */
+    protected $user;
+
+    /**
+     * @param bool $getNew
+     * @return User
+     */
+    public function getUser($getNew = false)
+    {
+        if (!$this->user) {
+            $user = User::create([
+                "login"      => "testUser_" . str_random(10),
+                "email"      => "testEmail" . str_random(7) . "@domain.test",
+                "first_name" => "testName" . str_random(5),
+                "api_token"  => str_random(30),
+                "password"   => Hash::make('1234')
+            ]);
+
+            if ($getNew) {
+                return $user;
+            }
+
+            $this->user = $user->refresh();
+        }
+        return $this->user;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function cleanUp() {
+        if ($this->user) {
+            $this->user->delete();
+        }
+    }
+}
