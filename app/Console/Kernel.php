33ea1eae90e987ba94358d13419336fe49804985
Max Kovalenko <mxss1998@yandex.ru>
2019-02-11 16:45:22 +0300

    email testing console command added to Console Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Console\Commands\Auth\GenRegisterKey;
+use App\Console\Commands\Testing\MailTesting;
 use App\Jobs\HomeworkStatuses\UpdateHomeworkStatusesJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -16,7 +17,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        GenRegisterKey::class
+        GenRegisterKey::class,
+        MailTesting::class
     ];
 
     /**
